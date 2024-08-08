c3a48a80e44dcb2af5ec08d9f73126400676e560
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-08 12:11:15 +0200

    Use .map()

--- src/components/BlogPage.tsx
+++ src/components/BlogPage.tsx
@@ -1,11 +1,49 @@
 import Blog from "./Blog.tsx";
 
+const Travel = [
+    {
+        country: "Turkey",
+        city: "Cappadocia",
+        date: "17.06.2023"
+    },
+    {
+        country: "Germany",
+        city: "Stuttgart",
+        date: "20.12.2024"
+    },
+    {
+        country: "Germany",
+        city: "Stuttgart",
+        date: "20.12.2024"
+    },
+    {
+        country: "Germany",
+        city: "Stuttgart",
+        date: "20.12.2024"
+    },
+    {
+        country: "Germany",
+        city: "Stuttgart",
+        date: "20.12.2024"
+    }
+
+]
+
 export default function BlogPage() {
 
     return (
         <div>
-            <Blog country={"Turkey"} city={"Cappadocia"} date={"17.06.2023"}/>
-            <Blog country={"Germany"} city={"Stuttgart"} date={"20.12.2024"}/>
+            {Travel.map((travel) => (
+                <Blog
+                    key={travel.city}
+                    country={travel.country}
+                    city={travel.city}
+                    date={travel.date}
+                />
+            ))};
+
+            {/*<Blog country={"Turkey"} city={"Cappadocia"} date={"17.06.2023"}/>
+            <Blog country={"Germany"} city={"Stuttgart"} date={"20.12.2024"}/>*/}
         </div>
     )
 }
